5284ff3a1fcda1dffd540e0d6e5deccd708ae1c7
Ritik <ritik.cse18@chitkarauniversity.edu.in>
2020-04-08 23:26:57 +0530

    Server side and DB management is done

---
+++ server.js
@@ -0,0 +1,46 @@
+
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+
+// const cors = require("cors");
+
+const api = require("./myApp");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+// Basic Configuration
+require('dotenv').config()
+
+const port = process.env.PORT || 3000;
+
+
+mongoose.connect(
+  process.env.URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+mongoose.connection.once("open", () => {
+  console.log("Db Connection");
+});
+// app.use(cors());
+
+app.use(cors());
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+
+api(app);
+app.listen(port, function() {
+  console.log("Node.js listening ...");
+});
